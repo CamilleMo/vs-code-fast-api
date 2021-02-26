fdde806115a95eb3bbde190a4a1e38434e747f46
Camille Moatti <camille.moatti@gmail.com>
2021-02-26 19:48:51 +0000

    all for api dev

--- app.py
+++ app.py
@@ -3,9 +3,17 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
-from flask import Flask
-app = Flask(__name__)
+from fastapi import FastAPI
+import uvicorn
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
